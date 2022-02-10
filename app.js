a8581d5258853f2315436cff8f9c56137a809a1d
James Etchells <jaw.etch@gmail.com>
2022-02-10 11:01:13 -0800

    Add reset.css file Add script to display myLibrary on form submit Add css to display and card elements

--- app.js
+++ app.js
@@ -7,19 +7,45 @@ function Book(title, author, pages, read) {
     this.author = author;
     this.pages = pages;
     this.read = read;
-
-    this.info = function() {
-        console.log(`${this.title} by ${this.author} has ${this.read}`)
-    }
 };
+
+Book.prototype.info = function () {
+    return console.log(`${this.title} by ${this.author}, is ${pages} pages, and has ${read}`);
+}
 
 function handleSubmit(e) {
     const data = new Book(this.title.value, this.author.value, this.pages.value, this.checkbox.value);
     myLibrary.push(data);
     console.log(myLibrary);
+    displayLibrary();
 
-    
     e.preventDefault();
+}
+
+function displayLibrary() {
+    const card = document.createElement("div");
+    const cardTitle = document.createElement('h1');
+    const cardAuthor = document.createElement('h2');
+    const cardPages = document.createElement('span');
+    const bookRead = document.createElement('span');
+
+    for (i = 0; i < myLibrary.length; i++) {
+
+        cardTitle.innerHTML = myLibrary[i].title;
+        cardAuthor.innerHTML = myLibrary[i].author;
+        cardPages.innerHTML = myLibrary[i].pages;
+        bookRead.innerHTML = myLibrary[i].read;
+
+        card.setAttribute('class', 'card');
+        card.appendChild(cardTitle);
+        card.appendChild(cardAuthor);
+        card.appendChild(cardPages);
+        card.appendChild(bookRead);
+
+        document.getElementById('display').appendChild(card);
+
+        console.log('hi');
+    }
 }
 
 
